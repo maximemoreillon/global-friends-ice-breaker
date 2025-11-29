492a084654a413ce2d177d5f63172d1351d8ec2d
Maxime Moreillon <m.moreillon@gmail.com>
2025-11-29 17:19:52 +0900

    config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { writable } from "svelte/store";
 import { getAuth, type User } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCv0JfyyMpxvlOCHhISeKIGoNcyx_ELqDg",
-  authDomain: "global-friends-countries.firebaseapp.com",
-  projectId: "global-friends-countries",
-  storageBucket: "global-friends-countries.appspot.com",
-  messagingSenderId: "1023646948861",
-  appId: "1:1023646948861:web:407fbb5f910f8f8a6acd94",
+  apiKey: "AIzaSyD__aqc8LZ1N5IgKFb-3mAHf2qmsWWGGLQ",
+  authDomain: "global-friends-ice-breaker.firebaseapp.com",
+  projectId: "global-friends-ice-breaker",
+  storageBucket: "global-friends-ice-breaker.firebasestorage.app",
+  messagingSenderId: "171279887110",
+  appId: "1:171279887110:web:5a745977ab60f0496f4c87",
 };
 
 // Initialize Firebase
